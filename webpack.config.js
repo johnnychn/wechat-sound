561ecc60e1189960aed7408ecfbf9fb78b542b0d
Johnny <157781745@qq.com>
2017-12-25 16:44:33 +0800

    remove dependens

--- webpack.config.js
+++ webpack.config.js
@@ -12,18 +12,11 @@ module.exports = {
 
   module: {
     loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
+
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
       },
       {
         test: /\.html$/,
