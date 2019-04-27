689f6fe20bde415b77df81d93aaee5fbbe7ad949
johnny <johnny@datwork.cn>
2019-04-27 19:54:32 +0800

    update init method

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 /**
  * Created by johnny on 2017/12/25.
  */
-function WechatSound(doc) {
+function WechatSound() {
+    if (!window.___inited_wechat_sound) {
+
+
+    window.___inited_wechat_sound = true;
+
     // 原理：调用链中的某个事件被标识为用户事件而非系统事件
     // 进而导致浏览器以为是用户触发播放而允许播放
 
     Audio.prototype._play = Audio.prototype.play;
     HTMLAudioElement.prototype._play = HTMLAudioElement.prototype.play;
     //Video.prototype._play = Video.prototype.play;
-    HTMLVideoElement.prototype._play=HTMLVideoElement.prototype.play;
+    HTMLVideoElement.prototype._play = HTMLVideoElement.prototype.play;
+
     function wxPlayAudio(audio) {
         /// <summary>
         /// 微信播放Hack
@@ -21,7 +27,13 @@ function WechatSound(doc) {
 
     function play() {
         var self = this;
-        self._play();
+        try{
+            self._play();
+        }catch(e){
+            console.log(e)
+
+        }
+
         var evtFns = [];
         try {
             wxPlayAudio(self);
@@ -35,42 +47,44 @@ function WechatSound(doc) {
     }
     Audio.prototype.play = play;
     HTMLAudioElement.prototype.play = play;
-    HTMLVideoElement.prototype.play=play;
+    HTMLVideoElement.prototype.play = play;
+    }
     return this;
 }
-WechatSound.prototype.preloads={}
-WechatSound.prototype.preloadVideo=function(url,playtime){
-    playtime=playtime||100;
-    var videoObj=document.createElement('video')
+
+WechatSound.prototype.preloads = {}
+WechatSound.prototype.preloadVideo = function (url, playtime) {
+    playtime = playtime || 100;
+    var videoObj = document.createElement('video')
     videoObj.setAttribute('webkit-playsinline', true);
     videoObj.setAttribute('playsinline', true);
     videoObj.setAttribute('preload', 'load');
     videoObj.setAttribute('src', url);
-    this.preloads[url]=videoObj;
+    this.preloads[url] = videoObj;
     videoObj.oncanplay = function () {
         setTimeout(function () {
-          //     console.log('canplay:'+url);
+            //     console.log('canplay:'+url);
             videoObj.pause();
-        },playtime)
+        }, playtime)
 
     }
     videoObj.play();
 };
-WechatSound.prototype.preloadAudio=function(url,playtime){
-    playtime=playtime||100;
-    var videoObj=new Audio()
+WechatSound.prototype.preloadAudio = function (url, playtime) {
+    playtime = playtime || 100;
+    var videoObj = new Audio()
     videoObj.setAttribute('preload', 'load');
     videoObj.setAttribute('src', url);
-    this.preloads[url]=videoObj;
+    this.preloads[url] = videoObj;
     videoObj.oncanplay = function () {
         setTimeout(function () {
             videoObj.pause();
-        },playtime)
+        }, playtime)
 
     }
     videoObj.play();
 };
-
-module.exports=WechatSound;
- window.WechatSound=WechatSound;
+WechatSound(document);
+module.exports = WechatSound;
+window.WechatSound = WechatSound;
 //WechatSound(document);
